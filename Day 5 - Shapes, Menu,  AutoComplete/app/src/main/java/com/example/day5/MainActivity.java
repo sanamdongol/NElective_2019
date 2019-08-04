b1c1c6ff25a9a1504d6d1b8ecfd47c592aae0188
sanam <sanam.dongol@gmail.com>
2019-08-04 07:51:02 +0545

    Missing menu file added

--- Day 5 - Shapes, Menu,  AutoComplete/app/src/main/java/com/example/day5/MainActivity.java
+++ Day 5 - Shapes, Menu,  AutoComplete/app/src/main/java/com/example/day5/MainActivity.java
@@ -1,17 +1,18 @@
-package com.example.day5;
+package com.example.menuplus;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
-    String[] countryList = {"Australia", "Algeria", "America", "Austria",
-            "Bangladesh", "Bolivia", "Bhutan", "China", "Chile", "Cambodia",
-            "Canada",
-            "Czech Republic", "Denmark", "Nepal"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +20,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        AutoCompleteTextView autoCompleteTv = findViewById(R.id.autoComplete);
+        Button btnPopUp = findViewById(R.id.btnPopUp);
+        btnPopUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopUpMenu(view);
+            }
+        });
+    }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countryList);
-        autoCompleteTv.setThreshold(1);
-        autoCompleteTv.setAdapter(adapter);
+    private void showPopUpMenu(View view) {
+       /* PopupMenu popup = new PopupMenu(this, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.game_menu, popup.getMenu());
+        popup.show();*/
+
+        PopupMenu popup = new PopupMenu(this, view);
+
+        // This activity implements OnMenuItemClickListener
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.game_menu);
+        popup.show();
 
     }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.new_game:
+                //toast
+                Toast.makeText(this, "First menu", Toast.LENGTH_SHORT).show();
+            case R.id.help:
+                //toast
+                return true;
+            default:
+                return false;
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+      switch (item.getItemId()) {
+            case R.id.new_game:
+                //toast
+                Toast.makeText(this, "First menu", Toast.LENGTH_SHORT).show();
+            case R.id.help:
+                //toast
+                return true;
+            default:
+                return super.onOptionsItemSelected(item); //returns false
+        }
+    }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
